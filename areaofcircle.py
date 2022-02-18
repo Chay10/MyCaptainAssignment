7e4e1002d8df49f561bc9e1df8e8a7d0bdee25bc
Chay10 <98901677+Chay10@users.noreply.github.com>
2022-02-18 18:56:46 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#AREA OF A CIRCLE
+
+pi=3.14
+x=float(input("Input the radius of the circle"))
+a=pi*x*x
+print("The area of the circle with radius = %.2f" %a)
+print(a)
+
+
