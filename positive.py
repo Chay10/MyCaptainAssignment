58a1266b47bc8288ec256590ca49cce155b740d9
Chay10 <98901677+Chay10@users.noreply.github.com>
2022-02-18 20:13:07 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 20:04:01 2022
+
+@author: HTM
+"""
+
+list = [1,2,-1,-2,4,-3,7,-9]
+
+for num in list:
+    if num>=0:
+        print(num, end=" ")
+    
