c9553644605e6a7e044925f30697a72717aa2a30
Chay10 <98901677+Chay10@users.noreply.github.com>
2022-02-18 20:02:18 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 19:30:05 2022
+
+@author: HTM
+"""
+
+n=int(input("Enter the value of n:"))
+a=0
+b=1
+sum=0
+count=1
+while(count<=n):
+    print(sum,end="")
+    count+=1
+    a=b
+    b=sum
+    sum=a+b
