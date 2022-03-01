fba2927b226258263922d9d30d93f6dec5b79bf5
Chay10 <98901677+Chay10@users.noreply.github.com>
2022-03-01 17:28:50 +0530

    Add files via upload

---
+++ extension1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 18:59:38 2022
+
+@author: HTM
+"""
+
+fn= input("Enter Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
