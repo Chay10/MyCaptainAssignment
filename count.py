15de6c7a8f8e27955d1583090ccd81a9b0146b96
Chay10 <98901677+Chay10@users.noreply.github.com>
2022-03-01 17:35:54 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 22:15:59 2022
+
+@author: HTM
+"""
+
+str ="mississippi"
+
+def make_dict(x):
+    dictionary={}
+    for letter in x:
+        dictionary[letter]=1+ dictionary.get(letter,0)
+    return dictionary
+
+def most_frequent(str):
+    letters=[letter.lower() for letter in str if letter.isalpha()]
+    dictionary=make_dict(letters)
+    result=[]
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print(letter, count)
+        
+most_frequent(str)
