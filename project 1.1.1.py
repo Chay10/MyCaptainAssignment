847108f918593dbb77a45014fcb28b8c1696019e
Chay10 <98901677+Chay10@users.noreply.github.com>
2022-03-01 18:41:31 +0530

    Add files via upload

---
+++ project 1.1.1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 18:26:09 2022
+
+@author: HTM
+"""
+
+x = 2
+y = 2
+#Operators
+#Arithmetic operators
+print(x*y)
+print(x+y)
+print(x-y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+
+
+
+
+
+
+
+
+
+
+
