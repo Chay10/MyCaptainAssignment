a901f0e5a5869dea62636654321fca8d18644f20
Chay10 <98901677+Chay10@users.noreply.github.com>
2022-03-01 18:25:38 +0530

    Add files via upload

---
+++ Project 1.2.2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 18:24:35 2022
+
+@author: HTM
+"""
+
+print("1.addition")
+print("2.subtraction")
+print("3.multiplication")
+print("4.division")
+print("5.modulus")
+print("6.exponentiation")
+print("7.floor division")
+
+choice=int(input("enter your choice:"))
+num1=int(input("enter first no."))
+num2=int(input("enter second no."))
+if(choice==1):
+   print("addition of two numbers=",num1+num2)
+elif(choice==2):
+   print("subtraction of two numbers=",num1-num2)
+elif(choice==3):
+   print("multiplication of two numbers=",num1*num2)
+elif(choice==4):
+    print("division of two numbers=",num1/num2)
+elif(choice==5):
+    print("modulus of two numbers=",num1%num2)
+elif(choice==6):
+    print("exponentiation of two numbers=",num1**num2)
+else:
+    print("floor division of two numbers=",num1//num2)
